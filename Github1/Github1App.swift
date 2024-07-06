96409e6c8fc7715c484ea76b1e2c185e60462f8a
XiYueGong <158237227+XiYueGong@users.noreply.github.com>
2024-07-06 11:34:27 +0800

    Initial Commit

---
+++ Github1/Github1App.swift
@@ -0,0 +1,17 @@
+//
+//  Github1App.swift
+//  Github1
+//
+//  Created by Xi yue Gong on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Github1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
